8a4708f8728d74e7ff9d3efc46f95fd654519251
ReneChamorro <rchamorrocrugeiras@danielcastelao.org>
2026-03-02 14:45:30 +0100

    Empezando objetos

--- juego.py
+++ juego.py
@@ -1,2 +1,3 @@
 class Juego:
-    def __init__(self):
+    def __init__(self, tablero):
+        self.tablero = tablero
--- tablero.py
+++ tablero.py
@@ -1,2 +1,6 @@
 class Tablero:
     def __init__(self):
+        self.posiciones = {}
+
+    def colocar_nave(self, x, y, nave):
+        self.posiciones[(x, y)] = nave
