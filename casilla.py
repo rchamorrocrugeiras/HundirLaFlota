0b05534cb9bf3c7509f498baa8b5690868af9d3e
ReneChamorro <rchamorrocrugeiras@danielcastelao.org>
2026-03-24 13:58:58 +0100

    Añadidos comentarios en la clase Casilla

--- casilla.py
+++ casilla.py
@@ -1,19 +1,22 @@
+# Creo la clase Casilla
 class Casilla:
     def __init__(self):
+        # Defino con estado a nave y a visitada
         self.nave = None
         self.visitada = False
 
+    # Creo el metodo para saber si ya se habia disparado a esa casilla antes
     def disparar(self):
-        if self.visitada:
+        if self.visitada:       # Si visitada es False
             print("Ya has disparado a esta casilla.")
-            return None
+            return None         # Devuelve que esa casilla ya habia sido disparada
 
-        self.visitada = True
+        self.visitada = True    # Cambiamos el estado
 
+        # Hacemos un condicional para que muestre el estado del lanzamiento
         if self.nave is None:
             print("Agua")
             return 0
-
         else:
             resultado = self.nave.recibir_disparo()
             if resultado == 2:
